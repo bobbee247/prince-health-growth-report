fea17a9c23fdb2d9b884693a9a859cdb6653e596
bobbee247 <bobbeee@gmail.com>
2026-04-02 15:35:33 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Prince Health - 6-Month Organic Growth Report',
+  description: '6-Month Organic Growth Report for Prince Health and Wellness, The Woodlands TX',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
